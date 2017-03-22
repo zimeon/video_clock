5c534ca5bd6e48c233ac5098672403cdebb4cdc6
Simeon Warner <simeon.warner@cornell.edu>
2017-03-21 21:51:45 -0400

    Add frame number, tidy

--- README.md
+++ README.md
@@ -5,12 +5,14 @@ Generate a video file of a clock.
 ## Requires
 
   * `ffmpeg`
-  * a bunch of python installs
+  * `Pillow` fork of the `PIL` Python image library (`pip install Pillow`)
 
 ## Usage
 
-To generate a 60 second video at 25 frames per second:
+To generate a 60 second video at 24 frames per second:
 
 ```
-python make_clock_video.py -d 60 -f 25 --outfile clock_60s.mp4
+python make_clock_video.py -d 60 -f 24 --outfile clock_60s.mp4
 ```
+
+The font included `DroidSansMono.ttf` is under [Apache 2](http://www.apache.org/licenses/LICENSE-2.0) license.
--- make_clock_video.py
+++ make_clock_video.py
@@ -7,21 +7,22 @@ import os.path
 import subprocess
 
 
-def make_images(frame_rate, duration, tmp="tmp"):
+def make_images(frame_rate, duration, width, height):
     """Make a set of images."""
-    subprocess.run("rm tmp/frame????.png", shell=True)
-    font = ImageFont.truetype("ClearSans-Bold.ttf", 50)
+    subprocess.run("rm tmp/frame*.png", shell=True)
+    font = ImageFont.truetype("DroidSansMono.ttf", 50)
     num_frames = int(duration * frame_rate) + 1
     for n in range(0, num_frames):
-        t = n / frame_rate
-        tsec = int(t)
-        t_str = ("%02d:%02d:%02d.%02d" %
-                 (tsec // 3600, (tsec // 60) % 60,
-                  tsec % 60, int((t - tsec) * 100)))
+        t_msec = int(n / frame_rate * 1000 + 0.5)
+        t_sec = t_msec // 1000
+        t_str = ("%02d:%02d:%02d.%03d" %
+                 (t_sec // 3600, (t_sec // 60) % 60,
+                  t_sec % 60, (t_msec - t_sec * 1000)))
         image_filename = ("tmp/frame%05d.png" % (n))
-        print("%s  %s" % (image_filename, t_str))
-        image = Image.new("RGBA", (310, 74), (255, 255, 255))
+        print("%s  %d  %s" % (image_filename, n, t_str))
+        image = Image.new("RGBA", (width, height), (255, 255, 255))
         ImageDraw.Draw(image).text((10, 0), t_str, (0, 0, 0), font=font)
+        ImageDraw.Draw(image).text((10, 55), ("frame %d" % n), (0, 0, 0), font=font)
         image.save(image_filename, "PNG")
 
 
@@ -30,6 +31,10 @@ p.add_option('--frame-rate', '-f', type='float', action='store',
              help='frame rate in frames per second')
 p.add_option('--duration', '-d', type='float', action='store',
              help='duration of video in second')
+p.add_option('--width', type='int', default='400',
+             help='image width (clock will be top left)')
+p.add_option('--height', type='int', default='120',
+             help='image height (clock will be top right)')
 p.add_option('--just-frames', '-j', action='store_true',
              help='just generate frames and then stop')
 p.add_option('--outfile', action='store', default='clock.mp4',
@@ -38,14 +43,13 @@ p.add_option('--outfile', action='store', default='clock.mp4',
 if (not opts.frame_rate or not opts.duration):
     p.error("Must specify frame rate and duration (see -h)")
 
-make_images(opts.frame_rate, opts.duration)
+make_images(opts.frame_rate, opts.duration, opts.width, opts.height)
 if (opts.just_frames):
     p.error("Frames generated, stopping")
 if (os.path.exists(opts.outfile)):
     os.remove(opts.outfile)
 subprocess.run(['ffmpeg',
                 '-r', str(opts.frame_rate),
-                '-s', '310x74',
                 '-i', 'tmp/frame%05d.png',
                 '-vcodec', 'libx264',
                 '-crf', '25',
